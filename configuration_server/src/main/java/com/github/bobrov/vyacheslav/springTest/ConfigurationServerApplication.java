97c6aa6f247851a8fc13e079dff546c1253dd225
Vyacheslav Bobrov <bobrov.vy@gmail.com>
2019-03-17 10:35:53 +0300

    Переименовал стартовый класс сервера конфигураций

--- configuration_server/src/main/java/com/github/bobrov/vyacheslav/springTest/ConfigurationServerApplication.java
+++ configuration_server/src/main/java/com/github/bobrov/vyacheslav/springTest/ConfigurationServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class SpringTestApplication {
+public class ConfigurationServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringTestApplication.class, args);
+		SpringApplication.run(ConfigurationServerApplication.class, args);
 	}
 
 }
