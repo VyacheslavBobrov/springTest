740688d3ab793fad089e5020cd9e7130a130e0ec
Vyacheslav Bobrov <bobrov.vy@gmail.com>
2019-03-18 15:49:32 +0300

    Отладил загрузку конфигурационных параметров

--- configuration_client/src/main/java/com/github/bobrov/vyacheslav/configuration_client/controllers/DefaultConfigurationController.java
+++ configuration_client/src/main/java/com/github/bobrov/vyacheslav/configuration_client/controllers/DefaultConfigurationController.java
@@ -1,39 +1,32 @@
 package com.github.bobrov.vyacheslav.configuration_client.controllers;
 
-import com.github.bobrov.vyacheslav.configuration_client.services.ConfigurationService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import static java.lang.String.format;
 
-@Controller
 @RefreshScope
+@RestController
 @Slf4j
 public class DefaultConfigurationController {
-    @Autowired
-    private ConfigurationService configurationService;
-
-    @Value("${param1}")
+    @Value("${param1:n/a}")
     private String param1;
 
-    @Value("${param2}")
+    @Value("${param2:n/a}")
     private String param2;
 
-    @Value("${param3}")
+    @Value("${param3:n/a}")
     private String param3;
 
-    @RequestMapping("/upload")
+    @RequestMapping
     public @ResponseBody
-    String upload(String configuration) throws RestException {
+    String showConfig() throws RestException {
         try {
-            configurationService.load(configuration);
-
             return format("val1=%s\nval2=%s\nval3=%s\nOK", param1, param2, param3);
         } catch (Exception e) {
             throw new RestException(e);
